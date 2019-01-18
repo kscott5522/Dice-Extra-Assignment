9a0e43000111d7a02064347671dd7c2c791bc9c7
kscott5522 <kscott5522@student.rcschools.nete>
2019-01-18 11:29:47 -0600

    bamba

---
+++ dice.py
@@ -0,0 +1,9 @@
+#Run a simulation of rolling five dice and see how many times a Yahtzee is rolled.
+#Calculate the percentage of rolling a Yahtzee
+
+
+import graphics as g
+import random
+
+
+def makeDice():
